44fda1c7ce39cf11d46946f27562fcbb1322e1d0
YOGEESH K P <yogeesh793@gmail.com>
2024-10-27 13:43:20 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,32 @@
+x=range(7)
+print(x)
+print(list(x))
+
+u=list(range(10,20))
+print(u)
+
+t=list(range(5,25,3))
+print(t)
+
+t=list(range(-50,6))
+print(t)
+
+t=list(range(-20,-25))
+print(t)
+
+x=list(range(-20,-25,-1))
+print(x)
+
+x=5 in range(10)
+print(x)
+
+x= -20 in range(2,5)
+print(x)
+
+x= -20 not in range(2,5)
+print(x)
+
+x= 50 not in range(10,100,5)
+print(x)
+
+
